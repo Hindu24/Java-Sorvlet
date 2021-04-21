bc202cf0539540338fa8a06aafc1f5ef06a02b4b
HINDU SHREE C T <44366777+Hindu24@users.noreply.github.com>
2021-04-21 22:11:59 +0530

    Add files via upload

---
+++ LoginInfo.java
@@ -0,0 +1,40 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/LoginInfo")
+public class LoginInfo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+    public LoginInfo() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+		protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String usname = (String)request.getParameter("username");
+		String pasw = (String)request.getParameter("password");
+		PrintWriter p = response.getWriter();
+		p.print("user:"+usname+" pass:"+pasw);
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+}
+
+
+
+
+
+	
